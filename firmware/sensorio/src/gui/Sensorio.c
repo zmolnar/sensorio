97bf71e2972b088899a8f9c36d4facbcc33b203b
Molnar <molnarxzoli@gmail.com>
2021-01-17 06:25:36 +0100

    Restructure GUI source files.

--- firmware/sensorio/src/gui/Sensorio.c
+++ firmware/sensorio/src/gui/Sensorio.c
@@ -1,24 +1,21 @@
 /**
- * @file LvglThread.cpp
+ * @file Sensorio.c
  * @brief
  */
 
 /*****************************************************************************/
 /* INCLUDES                                                                  */
 /*****************************************************************************/
-#include "LvglThread.h"
+#include "Sensorio.h"
 
-#include <Arduino.h>
+#include "Power.h"
 
-#include "drivers/encoder/Encoder.h"
-#include "drivers/lcd/SharpLcd.h"
-#include "gui/Sensorio.h"
-#include "lvgl.h"
+#include "core/LvglThread.h"
+#include "screens/Startup.h"
 
 /*****************************************************************************/
 /* DEFINED CONSTANTS                                                         */
 /*****************************************************************************/
-#define LVGL_TICK_IN_MS 5
 
 /*****************************************************************************/
 /* TYPE DEFINITIONS                                                          */
@@ -31,8 +28,6 @@
 /*****************************************************************************/
 /* DEFINITION OF GLOBAL CONSTANTS AND VARIABLES                              */
 /*****************************************************************************/
-static TaskHandle_t  lvglTask = NULL;
-static TimerHandle_t timerHandle;
 
 /*****************************************************************************/
 /* DECLARATION OF LOCAL FUNCTIONS                                            */
@@ -41,66 +36,19 @@ static TimerHandle_t timerHandle;
 /*****************************************************************************/
 /* DEFINITION OF LOCAL FUNCTIONS                                             */
 /*****************************************************************************/
-static void tick(TimerHandle_t xTimer)
-{
-  (void)xTimer;
-
-  static uint32_t counter = 0;
-  counter += LVGL_TICK_IN_MS;
-
-  if (SHARP_LCD_VCOM_CHANGE_PERIOD_IN_MS <= counter) {
-    SharpLcdToggleVcom();
-    counter = 0;
-  }
-
-  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-  vTaskNotifyGiveFromISR(lvglTask, &xHigherPriorityTaskWoken);
-  if (pdTRUE == xHigherPriorityTaskWoken) {
-    portYIELD_FROM_ISR();
-  }
-}
 
 /*****************************************************************************/
 /* DEFINITION OF GLOBAL FUNCTIONS                                            */
 /*****************************************************************************/
-void LvglThread(void *p)
+void SensorioStart(void)
 {
-  (void)p;
-
-  // Initialize LVGL
-  lv_init();
-
-  // Initialize display driver
-  SharpLcdInit();
-  SharpLcdRegisterDriver();
-
-  // Start GUI application
-  SensorioStart();
-
-  lvglTask = xTaskGetCurrentTaskHandle();
-  timerHandle = xTimerCreate(
-      "lvgl tick timer", pdMS_TO_TICKS(LVGL_TICK_IN_MS), pdTRUE, 0, tick);
-  if (pdPASS != xTimerStart(timerHandle, 0)) {
-    Serial.println("timer start failed");
-  }
-
-  while (1) {
-    uint32_t notification = ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-
-    if (0 < notification) {
-      lv_tick_inc(notification * LVGL_TICK_IN_MS);
-      lv_task_handler();
-      SharpLcdSendVcomIfNeeded();
-    } else {
-      Serial.println("lvgl timeout");
-    }
-  }
+  lv_obj_t *scr = startup_screen_create();
+  lv_scr_load(scr);
 }
 
-void LvglStartupFinished(void)
+void SensorioStartupFinished(void)
 {
-  EncoderInit();
-  EncoderRegisterDriver();
+  PowerStartupFinished();
 }
 
 /****************************** END OF FILE **********************************/
--- firmware/sensorio/src/gui/screens/Startup.c
+++ firmware/sensorio/src/gui/screens/Startup.c
@@ -1,24 +1,19 @@
 /**
- * @file LvglThread.cpp
+ * @file Startup.c
  * @brief
  */
 
 /*****************************************************************************/
 /* INCLUDES                                                                  */
 /*****************************************************************************/
-#include "LvglThread.h"
-
-#include <Arduino.h>
-
-#include "drivers/encoder/Encoder.h"
-#include "drivers/lcd/SharpLcd.h"
+#include "Startup.h"
 #include "gui/Sensorio.h"
-#include "lvgl.h"
+
+#include <stdint.h>
 
 /*****************************************************************************/
 /* DEFINED CONSTANTS                                                         */
 /*****************************************************************************/
-#define LVGL_TICK_IN_MS 5
 
 /*****************************************************************************/
 /* TYPE DEFINITIONS                                                          */
@@ -31,8 +26,7 @@
 /*****************************************************************************/
 /* DEFINITION OF GLOBAL CONSTANTS AND VARIABLES                              */
 /*****************************************************************************/
-static TaskHandle_t  lvglTask = NULL;
-static TimerHandle_t timerHandle;
+LV_IMG_DECLARE(paraglider);
 
 /*****************************************************************************/
 /* DECLARATION OF LOCAL FUNCTIONS                                            */
@@ -41,66 +35,45 @@ static TimerHandle_t timerHandle;
 /*****************************************************************************/
 /* DEFINITION OF LOCAL FUNCTIONS                                             */
 /*****************************************************************************/
-static void tick(TimerHandle_t xTimer)
+static void bar_increment(lv_task_t *t)
 {
-  (void)xTimer;
+  static int16_t a = 0;
 
-  static uint32_t counter = 0;
-  counter += LVGL_TICK_IN_MS;
+  a += 2;
+  lv_bar_set_value(t->user_data, a, LV_ANIM_ON);
 
-  if (SHARP_LCD_VCOM_CHANGE_PERIOD_IN_MS <= counter) {
-    SharpLcdToggleVcom();
-    counter = 0;
-  }
-
-  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-  vTaskNotifyGiveFromISR(lvglTask, &xHigherPriorityTaskWoken);
-  if (pdTRUE == xHigherPriorityTaskWoken) {
-    portYIELD_FROM_ISR();
+  if (100 <= a) {
+    a = 0;
+    lv_task_del(t);
+    SensorioStartupFinished();
   }
 }
 
 /*****************************************************************************/
 /* DEFINITION OF GLOBAL FUNCTIONS                                            */
 /*****************************************************************************/
-void LvglThread(void *p)
+lv_obj_t *startup_screen_create(void)
 {
-  (void)p;
+  lv_obj_t *scr = lv_obj_create(NULL, NULL);
 
-  // Initialize LVGL
-  lv_init();
+  lv_obj_t *cont = lv_cont_create(scr, NULL);
+  lv_obj_set_auto_realign(cont, true);
+  lv_obj_align_origo(cont, NULL, LV_ALIGN_CENTER, 0, 0);
+  lv_cont_set_fit(cont, LV_FIT_PARENT);
+  lv_cont_set_layout(cont, LV_LAYOUT_CENTER);
 
-  // Initialize display driver
-  SharpLcdInit();
-  SharpLcdRegisterDriver();
+  lv_obj_t *img = lv_img_create(scr, NULL);
+  lv_img_set_src(img, &paraglider);
+  lv_img_set_auto_size(img, true);
+  lv_obj_align(img, scr, LV_ALIGN_IN_TOP_MID, 0, 77);
 
-  // Start GUI application
-  SensorioStart();
+  lv_obj_t *bar = lv_bar_create(scr, NULL);
+  lv_obj_set_size(bar, 200, 15);
+  lv_obj_align(bar, scr, LV_ALIGN_IN_BOTTOM_MID, 0, -40);
+  lv_bar_set_value(bar, 0, LV_ANIM_OFF);
+  lv_task_create(bar_increment, 60, LV_TASK_PRIO_LOWEST, bar);
 
-  lvglTask = xTaskGetCurrentTaskHandle();
-  timerHandle = xTimerCreate(
-      "lvgl tick timer", pdMS_TO_TICKS(LVGL_TICK_IN_MS), pdTRUE, 0, tick);
-  if (pdPASS != xTimerStart(timerHandle, 0)) {
-    Serial.println("timer start failed");
-  }
-
-  while (1) {
-    uint32_t notification = ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-
-    if (0 < notification) {
-      lv_tick_inc(notification * LVGL_TICK_IN_MS);
-      lv_task_handler();
-      SharpLcdSendVcomIfNeeded();
-    } else {
-      Serial.println("lvgl timeout");
-    }
-  }
-}
-
-void LvglStartupFinished(void)
-{
-  EncoderInit();
-  EncoderRegisterDriver();
+  return scr;
 }
 
 /****************************** END OF FILE **********************************/
