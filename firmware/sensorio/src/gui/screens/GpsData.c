5b4cb1dbd6390f1b052eb77a9a02edebeb7e484c
Molnar <molnarxzoli@gmail.com>
2021-01-24 13:41:01 +0100

    Fix GPS data screen values.

--- firmware/sensorio/src/gui/screens/GpsData.c
+++ firmware/sensorio/src/gui/screens/GpsData.c
@@ -45,12 +45,12 @@ static void event_handler(lv_obj_t *obj, lv_event_t event)
 
       lv_table_set_cell_value_fmt(
           gpsdata, 0, 1, "%s", data.locked ? "Yes" : "No");
-      lv_table_set_cell_value_fmt(gpsdata, 1, 1, "%d m", data.altitude);
-      lv_table_set_cell_value_fmt(gpsdata, 2, 1, "%d", data.course);
+      lv_table_set_cell_value_fmt(gpsdata, 1, 1, "%.03f m", data.altitude / 1000.0);
+      lv_table_set_cell_value_fmt(gpsdata, 2, 1, "%.03f", data.course / 1000.0);
       lv_table_set_cell_value_fmt(gpsdata, 3, 1, "%d", data.latitude);
       lv_table_set_cell_value_fmt(gpsdata, 4, 1, "%d", data.longitude);
       lv_table_set_cell_value_fmt(gpsdata, 5, 1, "%d", data.numOfSatellites);
-      lv_table_set_cell_value_fmt(gpsdata, 6, 1, "%d", data.speed);
+      lv_table_set_cell_value_fmt(gpsdata, 6, 1, "%.03f m/s", data.speed / 1000.0);
       lv_table_set_cell_value_fmt(gpsdata,
                                   7,
                                   1,
